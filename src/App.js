687651cc3acdb077225acb63d039393747599057
Iyris <iyrisvigil@gmail.com>
2020-11-24 22:37:34 -0800

    add songs on side bar  library and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+// Import styles
+import "./styles/app.scss";
+// Import music
+import chillHop from "./util";
+// Import components
 import Player from "./components/Player";
 import Song from "./components/Song";
-import "./styles/app.scss";
-import chillHop from "./util";
+import Library from "./components/Library";
 
 function App() {
   const [songs, setSongs] = useState(chillHop());
@@ -16,6 +20,7 @@ function App() {
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,14 @@
+const LibrarySong = ({ song }) => {
+  console.log(song);
+  return (
+    <div className="library-song">
+      <img src={song.cover} alt={song.name}></img>
+      <div className="song-description">
+        <h3> {song.name} </h3>
+        <h4> {song.artist} </h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
