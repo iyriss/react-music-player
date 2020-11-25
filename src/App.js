dc6e6d55239f1b4e28a0837a381f97f0afc4bacf
Iyris <iyrisvigil@gmail.com>
2020-11-24 20:59:04 -0800

    play, pause and update time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import chillHop from "./util";
 
 function App() {
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,5 @@
 // import React from "react";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -6,23 +7,66 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+  // Ref (instead of doing const audio = document.querySelector('audio'))
+  const audioRef = useRef(null); // and as we added it to audio as ref attribute now instead of null current is => {current: audio }
+
+  // Event Handlers
+  const playSongHandler = () => {
+    // console.log(audioRef.current); //output: <audio src="https://mp3.chillhop.com/serve.php/?mp3=10075"></audio>
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const timeUpdateHandler = (e) => {
+    console.log(e.target);
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime, duration });
+  };
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+      // first divide it by 60 to show the minute and after the seconds. Every time it gets up to 60 it starts back at 0
+    );
+  };
   return (
     <div className="player">
       <div className="time-control">
-        <p> Start Time </p>
+        <p> {getTime(songInfo.currentTime)} </p>
         <input type="range" />
-        <p> End Time </p>
+        <p> {getTime(songInfo.duration)} </p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="play" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
