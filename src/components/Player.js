639421d6078e31a4bb7e9d52b4bef0720aa76071
Iyris <iyrisvigil@gmail.com>
2020-11-28 22:18:06 -0800

    fix NaN error

--- src/components/Player.js
+++ src/components/Player.js
@@ -72,12 +72,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         <p> {getTime(songInfo.currentTime)} </p>
         <input
           min={0}
-          max={songInfo.duration}
+          max={songInfo.duration || 0}
           value={songInfo.currentTime}
           onChange={dragHandler}
           type="range"
         />
-        <p> {getTime(songInfo.duration)} </p>
+        {/* Added or zero below to fix NaN error */}
+        <p> {getTime(songInfo.duration || 0)} </p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
